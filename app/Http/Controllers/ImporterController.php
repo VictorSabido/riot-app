316e5e10970cddd0aa99d9c0da07d1b763c0d9dd
Víctor Sabido <vsabidorubio@gmail.com>
2020-05-13 22:24:29 +0200

    Method getChampions is now updatedOrCreate

--- app/Http/Controllers/ImporterController.php
+++ app/Http/Controllers/ImporterController.php
@@ -11,18 +11,21 @@ class ImporterController extends Controller
     public function getChampions() {
         $client = new \GuzzleHttp\Client();
         $championRequest = $client->request('GET', 'http://ddragon.leagueoflegends.com/cdn/10.9.1/data/en_US/champion.json');
+
         if($championRequest->getStatusCode() == 200){
             $champions = json_decode($championRequest->getBody()->getContents())->data;
             foreach($champions as $champion)
             {
-                $champ = new Champion;
-                $champ->champ_id = $champion->id;
-                $champ->key      = $champion->key;
-                $champ->name     = $champion->name;
-                $champ->image    = $champion->image->full;
-                $champ->version  = $champion->version;
-                
-                $champ->save();
+                Champion::updateOrCreate(
+                    ['champ_id' => $champion->id],
+                    [
+                        'champ_id' => $champion->id,
+                        'key'      => $champion->key,
+                        'name'     => $champion->name,
+                        'image'    => $champion->image->full,
+                        'version'  => $champion->version
+                    ]
+                );
             }
 
             return true;
--- app/Models/Champion.php
+++ app/Models/Champion.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Champion extends Model
 {
-    //
+    protected $fillable = ['champ_id', 'key', 'name', 'image', 'version'];
 }
