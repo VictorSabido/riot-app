381b22a7a45e44287c9a41e49cf1346252556182
Víctor Sabido <vsabidorubio@gmail.com>
2020-05-19 21:20:51 +0200

    Relationship w/leagues

--- app/Models/Summoner.php
+++ app/Models/Summoner.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\League;
 
 class Summoner extends Model
 {
     protected $fillable = ['summId', 'accountId', 'puuid', 'name', 'profileIconId', 'revisionDate', 'summonerLevel'];
+
+    public function leagues() {
+        return $this->hasMany(League::class, 'summoner_id');
+    }
 }
